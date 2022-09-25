2d70c2e539ea1027b52005f800abd75d9d0d369a
Pooja Sanwal <sanwalp20@gmail.com>
2022-09-26 05:08:02 +0530

    About popup added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import notesAPI from "./notesAPI.js"
+
+// --------- About popup -------
+
+let popup = document.getElementById("about-popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
