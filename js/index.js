8774b82cb6b371532a482bf098d83c2e7513fed9
Pooja Sanwal <sanwalp20@gmail.com>
2022-11-01 23:58:45 +0530

    About section text changes

--- js/index.js
+++ js/index.js
@@ -7,3 +7,32 @@
 //Other feature:
 // List and grid view
 
+//New note:
+//grabbing the sideview elements:
+
+const noteTitle = document.getElementsByClassName("note-title");
+const noteBody = document.getElementsByClassName("note-body");
+const noteDate = document.getElementsByClassName("note-date");
+
+//grabbing the list where all notes are saved
+
+const notesList = document.querySelector(".notes-list");
+
+//making an empty array to store notes list
+
+let notesArray = []
+let activeNote
+let newCreated = true
+
+// selecting the "new-note" icon
+
+const addNewNote = document.getElementById("new-note");
+
+
+// pushing new notes to the notesArray list
+
+addNewNote.addEventListener("click", ()=>{
+    let
+})
+
+//grabbing the text put in by the user and displaying it in the side-view
